5a040f663896da4c29e717b2285dfaa7dbace408
Naf_Second <98323709+Naf-Second@users.noreply.github.com>
2023-03-28 05:24:41 +0600

    Add files via upload

---
+++ Codeforces Checking.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        char x;
+        cin >> x;
+
+    if(x=='c' || x=='o' ||x=='d' || x=='e' || x=='f' || x=='o' || x=='r' || x=='c' || x=='e' || x=='s')
+        cout << "YES" << "\n";
+    else
+        cout << "No" << "\n";
+    }
+}
