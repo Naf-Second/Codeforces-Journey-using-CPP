e289fa645990256f313479487646d2a51ad25a3e
Naf_Second <98323709+Naf-Second@users.noreply.github.com>
2023-04-14 09:22:09 +0600

    Add files via upload

---
+++ Coins.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,k;
+        cin >> n >> k;
+        int x=0,y=0;
+        int z = 2*x+k*y;
+
+        if(n%2==0)
+            cout << "Yes" << "\n";
+        else if(n%2!=0 && k%2==0)
+        {
+            cout << "No" << "\n";
+        }
+        else
+            cout << "Yes" << "\n";
+
+
+
+
+}
+
+    }
